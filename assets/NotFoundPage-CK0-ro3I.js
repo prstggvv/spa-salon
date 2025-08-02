a10f4e486bd90501324f29ba24c883311297b524
Ivan <vany4golos@yandex.ru>
2025-08-02 21:53:33 +0700

    Updates

---
+++ assets/NotFoundPage-CK0-ro3I.js
@@ -0,0 +1,1 @@
+import{j as s,c as n}from"./index-Du5uMFNb.js";const o={},e=({className:t})=>s.jsx("main",{className:n(o.content,{},[t??""]),children:s.jsx("h1",{children:"Not Found"})});export{e as default};
