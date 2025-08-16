9dd41e9b2bac9f7ef9e81f4eae5cbec0c050acb6
Ivan <vany4golos@yandex.ru>
2025-08-17 03:35:38 +0700

    Updates

---
+++ assets/NotFoundPage-BcmQY53L.js
@@ -0,0 +1,1 @@
+import{j as s,c as a}from"./index-DtMPggJj.js";const e={},c=({className:t})=>s.jsx("main",{className:a(e.content,{},[t??""]),children:s.jsx("h1",{children:"404"})});export{c as default};
