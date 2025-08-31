295ed4286dc830652790ee2c6cd37f0c56328ceb
Ivan <vany4golos@yandex.ru>
2025-09-01 02:31:09 +0700

    Updates

--- assets/NotFoundPage-BWGHS8Hx.js
+++
@@ -1,1 +0,0 @@
-import{j as s,c as a}from"./index-My0aXKQh.js";const e={},c=({className:t})=>s.jsx("main",{className:a(e.content,{},[t??""]),children:s.jsx("h1",{children:"404"})});export{c as default};
