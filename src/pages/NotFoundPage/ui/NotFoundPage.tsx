cf517ef770501d2ae5cb7a68f1be93089f50e055
Ivan <vany4golos@yandex.ru>
2025-09-23 17:56:21 +0700

    fix: NotFoundPage

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,14 +1,29 @@
 import cls from './NotFoundPage.module.css';
 import { classNames } from '../../../shared/lib/classNames/classNames';
+import { useNavigate } from 'react-router-dom';
 
 interface INotFoundPageData {
   className?: string;
 }
 
 const NotFoundPage = ({ className }: INotFoundPageData) => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate('/');
+  }
+
   return (
     <main className={classNames(cls.content, {}, [className ?? ''])}>
-      <h1>404</h1>
+      <h1 className={classNames(cls.title, {}, [])}>404</h1>
+      <p className={classNames(cls.subtitle, {}, [])}>
+        Страница не найдена
+      </p>
+      <button 
+        onClick={handleBack}
+        className={classNames(cls.button, {}, [])}>
+          Назад
+      </button>
     </main>
   )
 };
