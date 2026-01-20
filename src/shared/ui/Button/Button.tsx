b424f6da59a283ae8548b5c983ff1ad7d6651523
Ivan <vany4golos@yandex.ru>
2026-01-20 16:45:48 +0300

    fix: button

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -18,6 +18,7 @@ const Button = ({
       className={classNames(cls.button, {}, [className || ''])}
       href={href}
       initial={{ opacity: 0, y: 10 }}
+      viewport={{ once: true }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6, delay: 0.2 }}
       target='_blank'
